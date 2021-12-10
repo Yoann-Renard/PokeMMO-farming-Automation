174b7529f17e85dfe7df7c235ff03d80d37f9ceb
Yoann-Renard <yoann.rnrd@gmail.com>
2021-12-10 14:09:22 +0100

    mvt fix

--- src/playerMovement.py
+++ src/playerMovement.py
@@ -1,6 +1,7 @@
 import random as rand
 import json
-
+import pyautogui
+import pydirectinput
 class playerMovement():
 
     def __init__(self) -> None:
@@ -10,17 +11,34 @@ class playerMovement():
             self.keyDown = config['down']
             self.keyLeft = config['left']
             self.keyRight = config['right']
+        self.sleep_delay = 0.1
 
     def up(self):
+        print('Up')
+        pydirectinput.press(self.keyUp)
+        pyautogui.sleep(self.sleep_delay)
+        pydirectinput.press(self.keyUp)
         return -2
 
     def down(self):
+        print('Down')
+        pydirectinput.press(self.keyDown)
+        pyautogui.sleep(self.sleep_delay)
+        pydirectinput.press(self.keyDown)
         return 2
 
     def left(self):
+        print('Left')
+        pydirectinput.press(self.keyLeft)
+        pyautogui.sleep(self.sleep_delay)
+        pydirectinput.press(self.keyLeft)
         return -1
 
     def right(self):
+        print('Right')
+        pydirectinput.press(self.keyRight)
+        pyautogui.sleep(self.sleep_delay)
+        pydirectinput.press(self.keyRight)
         return 1
 
     def rand_mv_square(self):
@@ -35,9 +53,9 @@ class playerMovement():
                 pos = pos + rand.choice([self.up, self.right])()
             elif pos == 4:
                 pos = pos + rand.choice([self.up, self.left])()
-            print(pos)
+            pyautogui.sleep(0.1)
 
 pmv = playerMovement()
 
-
 pmv.rand_mv_square()
+
